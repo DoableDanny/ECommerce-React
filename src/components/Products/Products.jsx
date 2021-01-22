f7212b26a90aee5a7fbfef83e2e5ae7272c48f12
Dan <dantheado@gmail.com>
2021-01-22 20:42:43 +0000

    Made Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
   {
@@ -24,8 +25,11 @@ const products = [
 
 // Grid item: xs=12 means 1 item takes whole space on xs screen. sm=6 means 2 items will fit on a row.
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify='center' spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
